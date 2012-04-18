94226af536949957b59a4c6da0ce3f401f5b4342
Christian <christian_freye@web.de>
2012-04-18 09:51:06 +0200

    added custom tabwidget

---
+++ qtwidgets/mytabwidget.h
@@ -0,0 +1,19 @@
+
+#ifndef MYTABWIDGET_H
+#define MYTABWIDGET_H
+
+#include <QTabWidget>
+
+class MyTabWidget : public QTabWidget
+{
+	Q_OBJECT
+
+	public:
+
+	MyTabWidget(QWidget* parent);
+	~MyTabWidget();
+
+
+};
+
+#endif
